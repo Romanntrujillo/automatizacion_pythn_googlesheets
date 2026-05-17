af3e13a8d6f21121f1cd9dd9e14b2e3cc9f6b4ef
Andres Fernandez <andres.fernandezcaballero@davinci.edu.ar>
2026-05-17 17:25:11 -0300

    genera historico de planilla

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-from planillas_google import acceder_planilla_google,update_planilla_google #acceder_planillas = ENCAPSULAMIENTO
+from planillas_google import acceder_planilla_google, create_spreadsheet  # acceder_planillas = ENCAPSULAMIENTO
 from utils import restar_meses
-import pandas as pd
 
 df = acceder_planilla_google("Churn Prueba 2")
 #print(df.head())
 
 from utils import get_mes_id_now
 mes_id_en_curso = get_mes_id_now() #invocamos la función
-print(mes_id_en_curso)
+
 mes_id_restado = restar_meses(mes_id_en_curso,4) #4 = arumento / parámetro.
+
 df_historico = df[df["MES_ID"]<mes_id_restado]
-
-print(df_historico)
-
 df_actual = df[df["MES_ID"]>=mes_id_restado]
-print(df_actual)
 
-update_planilla_google(df_actual,"Churn Prueba 2")
+nombre_pestania_historico = str(restar_meses(mes_id_restado, 1))
+
+create_spreadsheet("Historico Prueba", nombre_pestania_historico, df_historico)
+
--- planillas_google.py
+++ planillas_google.py
@@ -1,63 +1,47 @@
 import pandas as pd
 import gspread
 from google.oauth2.service_account import Credentials
-def acceder_planilla_google(nombre_planilla):
 
-    '''
-
-    Pregunta a ChatGPT: Quiero usar Google Sheets con la API de Google
-    BOT: cuenta-drive-sheets-prueba@prueba-automatizacion-495320.iam.gserviceaccount.com
-
-    '''
-
-    # Alcances // SCOPES: Permisos a los que tenes acceso.
-    SCOPES = [
+def google_open_client():
+    scope = [
         "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive"
     ]
 
-    # Credenciales
-    creds = Credentials.from_service_account_file(
-        "credentials.json",
-        scopes=SCOPES
-    )
+    cred = Credentials.from_service_account_file("credentials.json", scopes=scope)
 
-    # Cliente
-    client = gspread.authorize(creds)
+    cliente_bot = gspread.authorize(cred)
+    return cliente_bot
 
-    # Abrir spreadsheet
-    spreadsheet = client.open(nombre_planilla)
+def acceder_planilla_google(archivo_excel, numero_planilla = 0 ):
+    client = google_open_client()
+    spreadsheet = client.open(archivo_excel)
 
-    # Abrir hoja
-    sheet = spreadsheet.sheet1
+    sheet = spreadsheet.get_worksheet(numero_planilla)
 
     data = sheet.get_all_records()
     df = pd.DataFrame(data)
     return df
 
-def update_planilla_google(df,nombre_planilla):
-    SCOPES = [
-        "https://www.googleapis.com/auth/spreadsheets",
-        "https://www.googleapis.com/auth/drive"
-    ]
+def update_planilla_google(df,archivo_excel, numero_planilla=0):
 
-    # Credenciales
-    creds = Credentials.from_service_account_file(
-        "credentials.json",
-        scopes=SCOPES
-    )
-
-    # Cliente
-    client = gspread.authorize(creds)
-
+    client = google_open_client()
     # Abrir spreadsheet
-    spreadsheet = client.open(nombre_planilla)
+    sheet = client.open(archivo_excel).get_worksheet(numero_planilla)
 
-    # Abrir hoja
-    sheet = spreadsheet.sheet1
     sheet.update(
     [df.columns.values.tolist()] +
     df.values.tolist()
     )
-
     print("planilla_actualizada")
+
+def create_spreadsheet(archivo_excel, nombre_pestania, df):
+    client = google_open_client()
+    spreadsheet = client.open(archivo_excel)
+
+    data = [df.columns.values.tolist()] + df.values.tolist()
+    rows = len(data) + 10
+    cols = len(df.columns) + 10
+
+    new_worksheet = spreadsheet.add_worksheet(nombre_pestania, rows, cols)
+    new_worksheet.update(data)
